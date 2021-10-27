d2d6a8378cbf6622b5df40be6be7d43206c35e0f
yjkim13 <sk940913@naver.com>
2021-10-27 23:52:49 +0900

    업로드 페이지 내용 수정2

--- client/src/components/views/UploadProductPage/UploadProductPage.js
+++ client/src/components/views/UploadProductPage/UploadProductPage.js
@@ -2,8 +2,14 @@ import React from 'react'
 
 function UploadProductPage() {
     return (
-        <div>
-            UploadProductPage!!
+        <div style={{ maxWidth: '700px', margin: '2rem auto'}}>
+            <div style={{ textAlign:'center', marginBottom: '2rem'}}>
+                <h2> 여행 상품 업로드</h2>
+        </div>
+        <form>
+
+
+        </form>
         </div>
     )
 }
