ee5aa4b294f8ff0a284fad419aec015a8512f0b4
yjkim13 <sk940913@naver.com>
2021-10-29 14:25:26 +0900

    이미지 파일을 서버로 보내기

---
+++ server/routes/product.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const router = express.Router();
+
+//=================================
+//             Product
+//=================================
+
+router.post('/image',(req,res)=>{
+
+    //가져온 이미지를 저장해주면 된다.
+
+    
+})
+
+module.exports = router;
