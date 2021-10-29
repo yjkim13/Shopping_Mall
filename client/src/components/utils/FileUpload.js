7bfc55b1268addf0ab26a78d3c810f274248e4af
yjkim13 <sk940913@naver.com>
2021-10-29 16:48:16 +0900

    multer를 이용한 이미지 저장

--- client/src/components/utils/FileUpload.js
+++ client/src/components/utils/FileUpload.js
@@ -1,11 +1,34 @@
-import React from 'react'
+import React ,{useState} from 'react'
 import Dropzone from 'react-dropzone'
 import {PlusOutlined} from '@ant-design/icons';
+import axios from 'axios';
 
 function FileUpload() {
+
+  const [Image, setImage] = useState([])
+  const dropHandler = (files) =>{
+
+    let formData = new FormData();
+    const config ={
+      header:{'content-type':'multipart/form-data'}
+    }
+    formData.append("file", files[0])
+
+    axios.post('/api/product/image',formData,config)
+    .then(response=>{
+      if(response.data.success){
+  
+        setImage([...Image,response.data.filePath])
+      }else{
+        alert("파일을 저장하는데 실패했습니다.")
+      }
+    })
+
+  }
+
     return (
         <div style={{display:'flex', justifyContent: 'space-between'}}>
-           <Dropzone onDrop={acceptedFiles => console.log(acceptedFiles)}>
+           <Dropzone onDrop={dropHandler}>
   {({getRootProps, getInputProps}) => (
 
       <div 
@@ -20,6 +43,17 @@ function FileUpload() {
     
   )}
 </Dropzone>
+
+
+<div style={{display:'flex', width:'350px',height:'240px',overflowX:'scroll'}}>
+{Image.map((image,index)=>(
+  <div key={index}>
+    <img style={{minWidth:'300px',width:'300px',height:'240px'}}
+    src={`http://localhost:5000/${image}`}
+    />
+    </div>
+))}
+</div>
         </div>
     )
 }
--- server/routes/product.js
+++ server/routes/product.js
@@ -1,15 +1,33 @@
 const express = require('express');
 const router = express.Router();
+const multer  = require('multer')
 
 //=================================
 //             Product
 //=================================
 
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, 'uploads/')
+    },
+    filename: function (req, file, cb) {
+      const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
+      cb(null,`${Date.now()}_${file.originalname}`)
+    }
+  })
+  
+  const upload = multer({ storage: storage }).single("file")
+
 router.post('/image',(req,res)=>{
 
     //가져온 이미지를 저장해주면 된다.
+    upload(req,res,(err)=>{
+        if(err) {
+            return res.json({ success: false, err})
+        }
+        return res.json({ success: true, filePath: res.req.file.path , fileName: res.req.file.filename})
+    })
 
-    
 })
 
 module.exports = router;
