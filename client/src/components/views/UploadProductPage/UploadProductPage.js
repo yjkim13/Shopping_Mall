b3ad80e6da667d770a67ada58b05b628476e7feb
yjkim13 <sk940913@naver.com>
2021-10-30 14:38:57 +0900

    DB에 있는 상품 가져오기

--- client/src/components/views/UploadProductPage/UploadProductPage.js
+++ client/src/components/views/UploadProductPage/UploadProductPage.js
@@ -1,6 +1,8 @@
 import React,{useState} from 'react'
 import {Typography, Button, Form, Input} from 'antd';
 import FileUpload from '../../utils/FileUpload';
+import Axios from 'axios';
+
 
 //const {Title} = Typography;
 const {TextArea} = Input;
@@ -16,7 +18,7 @@ const Continents = [
     
 ]
 
-function UploadProductPage() {
+function UploadProductPage(props) {
 
     const [Title, setTitle] = useState("")
     const [Description, setDescription] = useState("")
@@ -43,14 +45,39 @@ function UploadProductPage() {
     const updateImages =(newImages) => {
         setImages(newImages)
     }
+    const submitHandler = (event) =>{
+        
 
+        if(!Title || !Description || !Price|| !Continent||!Images) {
+            return alert(" 모든 값을 넣어주셔야 합니다.")
+        }
+        //서버에 채운 값들을 Request로 보낸다.
+
+        const body = {
+            writer: props.user.userData._id,
+            title: Title,
+            description : Description,
+            price: Price,
+            images: Images,
+            continent: Continent
+        }
+        Axios.post("/api/product",body)
+        .then(response => {
+            if(response.data.success) {
+                alert("상품 업로드에 성공했습니다.")
+                props.history.push('/');
+            } else{
+                alert("상품 업로드에 실패했습니다.")
+            }
+        })
+    }
 
     return (
         <div style={{ maxWidth: '700px', margin: '2rem auto'}}>
             <div style={{ textAlign:'center', marginBottom: '2rem'}}>
                 <h2> 여행 상품 업로드</h2>
         </div>
-        <Form>
+        <Form onSubmit={submitHandler}>
             {/*Drop Zone */}
 
             <FileUpload refreshFunction={updateImages}/>
@@ -69,14 +96,14 @@ function UploadProductPage() {
         <Input type="number" onChange={priceChangeHandler} value={Price}/>
         <br/>
         <br/>
-        <select onChange={continentChangeHandler} value={Continent}>
+        <select onChange={continentChangeHandler}>
             {Continents.map(item => (
-                 <option key={item.key} value={item.key}> {item.value} </option>
+                 <option key={item.Key} value={item.value}> {item.value} </option>
             ))}
         </select>
         <br/>
         <br/>
-        <Button>
+        <Button type="submit" onClick= {submitHandler}>
             확인
         </Button>
         </Form>
--- server/routes/product.js
+++ server/routes/product.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
-const multer  = require('multer')
+const multer  = require('multer');
+const {Product} = require('../models/Product');
 
 //=================================
 //             Product
@@ -28,6 +29,28 @@ router.post('/image',(req,res)=>{
         return res.json({ success: true, filePath: res.req.file.path , fileName: res.req.file.filename})
     })
 
+})
+
+router.post('/',(req,res)=>{
+
+    //받아온 정보들을 DB에 넣어준다.
+   const product = new Product(req.body)
+    product.save((err) => {
+        if(err) return res.status(400).json({success: false,err})
+        return res.status(200).json({success: true })
+    })
+    
+})
+
+router.post('/products',(req,res)=>{
+
+  //product collect에 있는 상품을 모두 가져오기.
+  Product.find()
+  .populate("writer")
+  .exec((err,productInfo)=> {
+    if(err) return res.status(400).json({success :false, err})
+    return res.status(200).json({success: true, productInfo})
+  })
 })
 
 module.exports = router;
