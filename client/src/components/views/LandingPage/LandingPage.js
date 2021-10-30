c3a45c7940be386fec70d3073be5e764a635fe19
yjkim13 <sk940913@naver.com>
2021-10-30 15:46:53 +0900

    랜딩페이지 카드 만들기

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,17 +1,23 @@
-import React,{useEffect} from 'react'
+import React,{useEffect, useState} from 'react'
 import { FaCode } from "react-icons/fa";
 import axios from 'axios';
+import {
+RocketOutlined,
+} from '@ant-design/icons';
+import Meta from 'antd/lib/card/Meta';
+import {Col, Row , Card} from 'antd'
 
 function LandingPage() {
 
-
+    const [Products, setProducts] = useState([])
     useEffect(() => {
 
      
         axios.post('/api/product/products')
         .then(response =>{
             if(response.data.success){
-                console.log(response.data);
+                console.log(response.data)
+                setProducts(response.data.productInfo)
             }else {
                 alert("상품을 가져오는데 실패했습니다.")
             }
@@ -19,11 +25,46 @@ function LandingPage() {
         
     }, [])
 
+    const renderCards = Products.map((product,index)=> {
+        console.log(product);
+        return <Col lg={6} md={8} sx={24} key = {index}>
+        <Card
+       
+            cover={<img style ={{width: '100%', maxHeight:'150px'}}src={`http://localhost:5000/${product.images[0]}`}/>}
+        >
+            <Meta 
+                title={product.title}
+                description={`$${product.price}`}
+            />
+        </Card>
+        </Col>
+    })
+  
+   
 
 
     return (
-   <div>
-       LandingPage
+   <div style = {{ width: '75%', margin: '3rem auto'}}>
+       <div style= {{ textAlign: 'center'}}>
+           <h2>이제 모두 여행을 떠나요!<RocketOutlined/></h2>
+       </div>
+            {/* Filter */}
+
+            {/* Search */}
+
+            {/* Cards */}
+        <Row gutter= '16'>
+        {renderCards}
+
+        </Row>
+            
+
+
+
+       <div style= {{display: 'flex', justifyContent: 'center'}}>
+           <button>더보기</button>
+       </div>
+
    </div>
     )
 }
