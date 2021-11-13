3c6eb49f4280a43f631bdfb91113239df70ad36d
yjkim13 <sk940913@naver.com>
2021-11-13 18:33:17 +0900

    Updates

---
+++ precache-manifest.3d98f36236b99e1217cd6a3433890b5c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d24064d55bd871ac5e597cfd216d5845",
+    "url": "/Shopping_Mall/index.html"
+  },
+  {
+    "revision": "ca9bfeaaa27886ce8eb4",
+    "url": "/Shopping_Mall/static/css/2.86d04cf6.chunk.css"
+  },
+  {
+    "revision": "c4eb3aefc678adc1102a",
+    "url": "/Shopping_Mall/static/css/main.4bd717ec.chunk.css"
+  },
+  {
+    "revision": "ca9bfeaaa27886ce8eb4",
+    "url": "/Shopping_Mall/static/js/2.42bb0f7b.chunk.js"
+  },
+  {
+    "revision": "4f160e29840e1f64ec2468f8c9029bf3",
+    "url": "/Shopping_Mall/static/js/2.42bb0f7b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c4eb3aefc678adc1102a",
+    "url": "/Shopping_Mall/static/js/main.981161ab.chunk.js"
+  },
+  {
+    "revision": "0111fabb476e7c8da5bc",
+    "url": "/Shopping_Mall/static/js/runtime-main.70043877.js"
+  }
+]);
